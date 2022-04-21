83cd5eb224e37e84b6c3e71ef6e66b69215fe076
brandonclark314 <brandonclark314@knights.ucf.edu>
2022-04-21 14:16:05 -0400

    fix last commit

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 path_folder = '/home/cap6412.student1/DINO/'
 data_folder = '/home/cap6412.student1/DINO/'
-saved_models_dir = '/home/cap6412.student1/saveModel/sp_loss'
+save_models_dir = '/home/cap6412.student1/saveModel/sp_loss/'
 class_mapping = '/home/cap6412.student1/DINO/kin700_classmap.json'
 frame_counts = '/home/cap6412.student1/DINO/kin700_frame_counts.json'
 logs = 'logs/'
--- main_dino.py
+++ main_dino.py
@@ -30,7 +30,7 @@ import torch.nn as nn
 import torch.distributed as dist
 import torch.backends.cudnn as cudnn
 import torch.nn.functional as F
-from torch.nn import MSELoss as MSE
+import torch.nn.MSELoss as MSE
 from torchvision import datasets, transforms
 from torchvision import models as torchvision_models
 
@@ -41,7 +41,6 @@ from vision_transformer import DINOHead
 from dl import basic_dataloader, collate_fn_train
 from vivit import ViViT
 import parameters as params
-import config as cfg
 
 torchvision_archs = sorted(name for name in torchvision_models.__dict__
     if name.islower() and not name.startswith("__")
@@ -169,8 +168,8 @@ def train_dino(args):
     # ============ building student and teacher networks ... ============
     # if the network is ViViT
     if args.arch.lower() == 'vivit':
-        student = ViViT(params.reso_h, args.patch_size, num_frames= args.num_frames, dim= args.token_features, depth= args.num_layers, heads= args.num_heads, classifier=False, patch_type='2d', type='student')
-        teacher = ViViT(params.reso_h, args.patch_size, num_frames= args.num_frames, dim= args.token_features, depth= args.num_layers, heads= args.num_heads, classifier=False, patch_type='2d', type='teacher')
+        student = ViViT(params.reso_h, args.patch_size, num_frames= args.num_frames, dim= args.token_features, depth= args.num_layers, heads= args.num_heads)
+        teacher = ViViT(params.reso_h, args.patch_size, num_frames= args.num_frames, dim= args.token_features, depth= args.num_layers, heads= args.num_heads)
         embed_dim = args.token_features
     else:
         print(f"Unknown architecture: {args.arch}")
@@ -272,7 +271,6 @@ def train_dino(args):
         optimizer=optimizer,
         fp16_scaler=fp16_scaler,
         dino_loss=dino_loss,
-        spatial_loss=spatial_loss
     )
     start_epoch = to_restore["epoch"]
 
@@ -284,7 +282,9 @@ def train_dino(args):
         #data_loader.sampler.set_epoch(epoch)
 
         # ============ training one epoch of DINO ... ============
-        train_stats, loss_val = train_one_epoch(student, teacher, teacher_without_ddp, dino_loss, spatial_loss, data_loader, optimizer, lr_schedule, wd_schedule, momentum_schedule, epoch, fp16_scaler, args)
+        train_stats, loss_val = train_one_epoch(student, teacher, teacher_without_ddp, dino_loss,
+            data_loader, optimizer, lr_schedule, wd_schedule, momentum_schedule,
+            epoch, fp16_scaler, args)
 
         # ============ writing logs ... ============
         save_dict = {
@@ -298,14 +298,13 @@ def train_dino(args):
         }
         if fp16_scaler is not None:
             save_dict['fp16_scaler'] = fp16_scaler.state_dict()
-        if not os.path.exists(cfg.save_model_dir):
-            os.makedir(cfg.save_model_dir)
+        
         if loss_val < best:
             best = loss_val
-            save_file_path = os.path.join(cfg.save_model_dir, 'model_{}_best_{}.pth'.format(epoch+1, str(loss_val)[:6]))
+            save_file_path = os.path.join('./train_models/sp_loss', 'model_{}_best_{}.pth'.format(epoch+1, str(loss_val)[:6]))
             torch.save(save_dict, save_file_path)
         if epoch+1 % 5 == 0:
-            save_file_path = os.path.join(cfg.save_model_dir, 'model_{}.pth'.format(epoch+1))
+            save_file_path = os.path.join('./train_models/sp_loss', 'model_{}.pth'.format(epoch+1))
             torch.save(save_dict, save_file_path)
 
         '''utils.save_on_master(save_dict, os.path.join(args.output_dir, 'checkpoint.pth'))
@@ -317,4 +316,193 @@ def train_dino(args):
             with (Path(args.output_dir) / "log.txt").open("a") as f:
                 f.write(json.dumps(log_stats) + "\n")'''
     total_time = time.time() - start_time
-    total_time_str = str(datetime.timedelta(seconds=i
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    print('Training time {}'.format(total_time_str))
+
+
+def train_one_epoch(student, teacher, teacher_without_ddp, dino_loss, spatial_loss, data_loader,
+                    optimizer, lr_schedule, wd_schedule, momentum_schedule,epoch,
+                    fp16_scaler, args):
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    header = 'Epoch: [{}/{}]'.format(epoch, args.epochs)
+    for it, clips in enumerate(metric_logger.log_every(data_loader, 10, header)):
+        if clips == None:
+            continue
+
+        # update weight decay and learning rate according to their schedule
+        it = len(data_loader) * epoch + it  # global training iteration
+        for i, param_group in enumerate(optimizer.param_groups):
+            param_group["lr"] = lr_schedule[it]
+            if i == 0:  # only the first group is regularized
+                param_group["weight_decay"] = wd_schedule[it]
+
+        clips = clips.cuda()
+        # teacher and student forward passes + compute dino loss
+        with torch.cuda.amp.autocast(fp16_scaler is not None):
+            teacher_output, teacher_attn = teacher(clips[:params.batch_size*2], 'teacher')  # only the global views pass through the teacher
+            student_output, student_attn = student(clips, 'student')
+            d_loss = dino_loss(student_output, teacher_output, epoch)
+            te_attn_la = teacher_attn[:,:8,:]
+            te_attn_lb = teacher_attn[:,8:,:]
+            st_attn = st_attn[params.batch_size*2:]
+
+            te_attn = torch.stack([te_attn_la[0], te_attn_la[1], te_attn_lb[0], te_attn_lb[1]], dim=0).view(4, -1)
+            st_attn = torch.stack([st_attn[0], st_attn[2], st_attn[1], st_attn[3]], dim=0).view(4, -1)
+            
+            sp_loss = spatial_loss(st_attn, te_attn)
+
+            loss = 0.5*d_loss + 0.5*sp_loss
+
+        if it % 25 == 0:
+            print(f'Loss for epoch {epoch+1}/{args.epochs} iteration {it} is {loss.item()}')
+
+        if not math.isfinite(loss.item()):
+            print("Loss is {}, stopping training".format(loss.item()), force=True)
+            sys.exit(1)
+
+        # student update
+        optimizer.zero_grad()
+        param_norms = None
+        if fp16_scaler is None:
+            loss.backward()
+            if args.clip_grad:
+                param_norms = utils.clip_gradients(student, args.clip_grad)
+            utils.cancel_gradients_last_layer(epoch, student,
+                                              args.freeze_last_layer)
+            optimizer.step()
+        else:
+            fp16_scaler.scale(loss).backward()
+            if args.clip_grad:
+                fp16_scaler.unscale_(optimizer)  # unscale the gradients of optimizer's assigned params in-place
+                param_norms = utils.clip_gradients(student, args.clip_grad)
+            utils.cancel_gradients_last_layer(epoch, student,
+                                              args.freeze_last_layer)
+            fp16_scaler.step(optimizer)
+            fp16_scaler.update()
+
+        # EMA update for the teacher
+        with torch.no_grad():
+            m = momentum_schedule[it]  # momentum parameter
+            for param_q, param_k in zip(student.module.parameters(), teacher_without_ddp.parameters()):
+                param_k.data.mul_(m).add_((1 - m) * param_q.detach().data)
+
+        # logging
+        torch.cuda.synchronize()
+        metric_logger.update(loss=loss.item())
+        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+        metric_logger.update(wd=optimizer.param_groups[0]["weight_decay"])
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}, loss.item()
+
+
+class DINOLoss(nn.Module):
+    def __init__(self, out_dim, ncrops, warmup_teacher_temp, teacher_temp,
+                 warmup_teacher_temp_epochs, nepochs, student_temp=0.1,
+                 center_momentum=0.9):
+        super().__init__()
+        self.student_temp = student_temp
+        self.center_momentum = center_momentum
+        self.ncrops = ncrops
+        self.register_buffer("center", torch.zeros(1, out_dim))
+        # we apply a warm up for the teacher temperature because
+        # a too high temperature makes the training instable at the beginning
+        self.teacher_temp_schedule = np.concatenate((
+            np.linspace(warmup_teacher_temp,
+                        teacher_temp, warmup_teacher_temp_epochs),
+            np.ones(nepochs - warmup_teacher_temp_epochs) * teacher_temp
+        ))
+
+    def forward(self, student_output, teacher_output, epoch):
+        """
+        Cross-entropy between softmax outputs of the teacher and student networks.
+        """
+        student_out = student_output / self.student_temp
+        #student_out = student_out.chunk(self.ncrops)
+
+        # teacher centering and sharpening
+        temp = self.teacher_temp_schedule[epoch]
+        teacher_out = F.softmax((teacher_output - self.center) / temp, dim=-1)
+        teacher_out = teacher_out.detach()#.chunk(2)
+
+        total_loss = 0
+        n_loss_terms = 0
+        for i, q in enumerate(teacher_out):
+            idxs = [x for x in range(i%params.batch_size,params.batch_size*6,params.batch_size)]
+            for j, v in enumerate(student_out):
+                if j in idxs and j != i:
+                    loss = torch.sum(-q * F.log_softmax(v, dim=-1), dim=-1)
+                    total_loss += loss.mean()
+                    
+                    n_loss_terms += 1
+        total_loss /= n_loss_terms
+        self.update_center(teacher_output)
+        return total_loss
+
+    @torch.no_grad()
+    def update_center(self, teacher_output):
+        """
+        Update center used for teacher output.
+        """
+        batch_center = torch.sum(teacher_output, dim=0, keepdim=True)
+        #dist.all_reduce(batch_center)
+        #batch_center = batch_center / (len(teacher_output) * dist.get_world_size())
+        batch_center = batch_center / len(teacher_output)
+        # ema update
+        self.center = self.center * self.center_momentum + batch_center * (1 - self.center_momentum)
+
+
+class DataAugmentationDINO(object):
+    def __init__(self, global_crops_scale, local_crops_scale, local_crops_number):
+        flip_and_color_jitter = transforms.Compose([
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomApply(
+                [transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)],
+                p=0.8
+            ),
+            transforms.RandomGrayscale(p=0.2),
+        ])
+        normalize = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ])
+
+        # first global crop
+        self.global_transfo1 = transforms.Compose([
+            transforms.RandomResizedCrop(224, scale=global_crops_scale, interpolation=Image.BICUBIC),
+            flip_and_color_jitter,
+            utils.GaussianBlur(1.0),
+            normalize,
+        ])
+        # second global crop
+        self.global_transfo2 = transforms.Compose([
+            transforms.RandomResizedCrop(224, scale=global_crops_scale, interpolation=Image.BICUBIC),
+            flip_and_color_jitter,
+            utils.GaussianBlur(0.1),
+            utils.Solarization(0.2),
+            normalize,
+        ])
+        # transformation for the local small crops
+        self.local_crops_number = local_crops_number
+        self.local_transfo = transforms.Compose([
+            transforms.RandomResizedCrop(96, scale=local_crops_scale, interpolation=Image.BICUBIC),
+            flip_and_color_jitter,
+            utils.GaussianBlur(p=0.5),
+            normalize,
+        ])
+
+    def __call__(self, image):
+        crops = []
+        crops.append(self.global_transfo1(image))
+        crops.append(self.global_transfo2(image))
+        for _ in range(self.local_crops_number):
+            crops.append(self.local_transfo(image))
+        return crops
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('DINO', parents=[get_args_parser()])
+    args = parser.parse_args()
+    Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+    train_dino(args)
